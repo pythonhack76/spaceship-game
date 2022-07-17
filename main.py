c1329a36d3e0069aef0870981abe15e4be707908
Python The Hack <lrulvoni@gmail.com>
2022-07-17 11:13:07 +0200

    update yellow & red

--- main.py
+++ main.py
@@ -20,14 +20,16 @@ YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESH
 #recupero immagini astranove rossa
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
 #dimensione e rotazione immagine rossa
-RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 #funzione creazione finestra
-def draw_window():
+def draw_window(red, yellow):
     
     #colore testata finestra
     WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP, (300, 100)) 
+    #posizione navicelle nella finestra di gioco
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y)) 
+    WIN.blit(RED_SPACESHIP, (red.x, red.y)) 
     #colore finestra
     pygame.display.update()
 
@@ -35,6 +37,9 @@ def draw_window():
 
 #funzione principale start
 def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -43,7 +48,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
       
-        draw_window()
+        red.x += 1
+        draw_window(red, yellow)
 
     pygame.quit()
 
