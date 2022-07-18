3ffef4e34646a62f578f0e552479bee358fa3c69
Python The Hack <lrulvoni@gmail.com>
2022-07-18 09:31:07 +0200

    update on github

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gamer")
 
 WHITE = ((255, 255, 255))
+BLACK = (0,0,0)
+
+BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
 
 FPS = 60
+VEL = 5
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40 
 
 #recupero immagini astranove gialla
@@ -27,6 +31,7 @@ def draw_window(red, yellow):
     
     #colore testata finestra
     WIN.fill(WHITE)
+    pygame.draw.rect(WIN, BLACK, BORDER)
     #posizione navicelle nella finestra di gioco
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y)) 
     WIN.blit(RED_SPACESHIP, (red.x, red.y)) 
@@ -34,6 +39,29 @@ def draw_window(red, yellow):
     pygame.display.update()
 
 
+def yellow_handle_movement(keys_pressed, yellow):
+    #gestione pulsanti movimento navicella
+    
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0 : #sinistra
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x: #destra
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0: #sopra
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15: #sotto
+        yellow.y += VEL
+
+def red_handle_movement(keys_pressed, red):
+    #gestione pulsanti movimento navicella
+    
+    if keys_pressed[pygame.K_LEFT]  and red.x - VEL > BORDER.x + BORDER.width : #sinistra
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH: #destra
+        red.x += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0: #sopra
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15: #sotto
+        red.y += VEL
 
 #funzione principale start
 def main():
@@ -47,8 +75,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-      
-        red.x += 1
+
+        #gestione pulsanti movimento navicella
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)  
+        red_handle_movement(keys_pressed, red)        
         draw_window(red, yellow)
 
     pygame.quit()
